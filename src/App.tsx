12120ad22243ae6a70915966809d2bf99ed4ec61
Dmytro Davidenko <dima080885@gmail.com>
2023-06-03 09:03:44 +0300

    ADD: basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Shop from './pages/Shop/Shop';
+import ShoppingCart from './pages/ShoppingCart/ShoppingCart';
+import { NavLink, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <nav>
+        <NavLink to="/" end>
+          Shop
+        </NavLink>
+        <NavLink to="/shopping-cart">Shopping Cart</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/shopping-cart" element={<ShoppingCart />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
